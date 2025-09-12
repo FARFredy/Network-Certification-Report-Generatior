9eb30f836c5ea3f6b30e86cde1b1cd5ebf4750e0
Portafolio <98476506+FARFredy@users.noreply.github.com>
2025-09-12 07:20:03 +0000

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  // Establece la base apuntando al subdirectorio de GitHub Pages
-  base: '/Network-Certification-Report-Generatior/',
-  plugins: [react()],
-})
